fde81e26f875f3d102d76ff67656f207d74d81d4
Edith Bautista <edith.bautista47@uabc.edu.mx>
2022-04-09 11:55:57 -0700

    calculadora

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CalculatorComponent implements OnInit {
 
-  currentNumber = '0';
+ currentNumber = '0';
  firstOperand:any|null = null;
  operator:any|null = null;
  waitForSecondNumber = false;
