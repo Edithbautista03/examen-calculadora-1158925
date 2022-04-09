0b3b78b8e87b0709aea727a6926144bd21cf0c55
Edith Bautista <edith.bautista47@uabc.edu.mx>
2022-04-09 11:46:26 -0700

    calculadora examen

---
+++ src/app/calculator/calculator.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+export class CalculatorComponent implements OnInit {
+
+  currentNumber = '0';
+ firstOperand:any|null = null;
+ operator:any|null = null;
+ waitForSecondNumber = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+}
